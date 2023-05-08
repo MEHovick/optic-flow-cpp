2746dfea395ded1afc8f150c4a9e11e5abcf1196
MEHovick <ilyastepanov.formal@gmail.com>
2023-05-08 22:45:15 +0500

    Update 08.05.22

--- main.cpp
+++ main.cpp
@@ -45,6 +45,7 @@ Mat flowVizualization(Mat &orig, Mat &flow, Mat &mask) {
 	Mat flow_magnitude, angle;
     vector<Mat> flow_channels;
     split(flow, flow_channels);
+	
     cartToPolar(flow_channels[0], flow_channels[1], flow_magnitude, angle, true);
 
     Mat flow_magnitude_1d = flow_magnitude.reshape(1, 1);
@@ -124,6 +125,8 @@ Mat flowVizualization(Mat &orig, Mat &flow, Mat &mask) {
 	magMask = magMaskCopy;
 	return img;
 
+	// Mat magn_norm;
+	// normalize(flow_magnitude, magn_norm, 0.0f, 1.0f, NORM_MINMAX);
     // angle *= ((1.f / 360.f) * (180.f / 255.f));
     // Mat _hsv[3], hsv, hsv8, bgr;
     // _hsv[0] = angle;
@@ -186,7 +189,7 @@ Mat DeepFlow(Mat &frameOld, Mat &frame, Mat &mask) {
 	return flowVizualization(frame, flow, mask);
 }
 
-Mat HornSchunckFlow(Mat &frameOld, Mat &frame) {
+Mat HornSchunckFlow(Mat &frameOld, Mat &frame, Mat &mask) {
     int iterations = 100, avg_window = 5; double alpha = 1;
 
     Mat img_gray, img2_gray;
@@ -194,8 +197,8 @@ Mat HornSchunckFlow(Mat &frameOld, Mat &frame) {
 	cvtColor(frame, img2_gray, COLOR_BGR2GRAY);
 
     Mat img_gray_db, img2_gray_db;
-    img_gray.convertTo(img_gray_db, CV_64FC1, 1.0 / 255.0);
-	img2_gray.convertTo(img2_gray_db, CV_64FC1, 1.0 / 255.0);
+    img_gray.convertTo(img_gray_db, CV_32F, 1.0 / 255.0);
+	img2_gray.convertTo(img2_gray_db, CV_32F, 1.0 / 255.0);
 
     Mat I_t = img2_gray_db - img_gray_db;
     Mat I_x, I_y;
@@ -203,9 +206,9 @@ Mat HornSchunckFlow(Mat &frameOld, Mat &frame) {
     Sobel(img_gray_db, I_x, ddepth, 1, 0, 3);
 	Sobel(img_gray_db, I_y, ddepth, 0, 1, 3);
 
-    Mat U = Mat::zeros(I_t.rows, I_t.cols, CV_64FC1);
-	Mat V = Mat::zeros(I_t.rows, I_t.cols, CV_64FC1);
-    Mat kernel = Mat::ones(avg_window, avg_window, CV_64FC1) / pow(avg_window, 2);
+    Mat U = Mat::zeros(I_t.rows, I_t.cols, CV_32F);
+	Mat V = Mat::zeros(I_t.rows, I_t.cols, CV_32F);
+    Mat kernel = Mat::ones(avg_window, avg_window, CV_32F) / pow(avg_window, 2);
 
     for (int i = 0; i < iterations; i++) {
 		
@@ -234,146 +237,24 @@ Mat HornSchunckFlow(Mat &frameOld, Mat &frame) {
 		U = U_avg - I_x_C;
 		V = V_avg - I_y_C;
 	}
+	vector<cv::Mat> flow_channels;
+	flow_channels.push_back(U);
+	flow_channels.push_back(V);
 
-    Mat img;
+	cv::Mat flow;
+	merge(flow_channels, flow);
 
-    int RY = 15;
-	int YG = 6;
-	int GC = 4;
-	int CB = 11;
-	int BM = 13;
-	int MR = 6;
+	int data_type = flow_channels[0].type();
+	int type_code = CV_MAT_TYPE(data_type);
+	string type_str = cv::typeToString(type_code);
 
-	int num_cols = RY + YG + GC + CB + BM + MR;
-	int col = 0;
-
-	Mat color_wheel = Mat::zeros(num_cols, 3, CV_64FC1);
-
-	//RY calculation
-	for (int i = 0; i < RY; i++) {
-		color_wheel.at<double>(i, 0) = 255;
-		color_wheel.at<double>(i, 1) = floor(255 * i / RY);
-	}
-	col += RY;
-
-	//YG calculation
-	for (int i = 0; i < YG; i++) {
-		color_wheel.at<double>(col + i, 0) = 255 - floor(255 * i / YG);
-		color_wheel.at<double>(col + i, 1) = 255;
-	}
-	col += YG;
-
-	//GC calculation
-	for (int i = 0; i < GC; i++) {
-		color_wheel.at<double>(col + i, 1) = 255;
-		color_wheel.at<double>(col + i, 2) = floor(255 * i / GC);
-	}
-	col += GC;
-
-	//CB calculation
-	for (int i = 0; i < CB; i++) {
-		color_wheel.at<double>(col + i, 1) = 255 - floor(255 * i / CB);
-		color_wheel.at<double>(col + i, 2) = 255;
-	}
-	col += CB;
-
-	//BM calculation
-	for (int i = 0; i < BM; i++) {
-		color_wheel.at<double>(col + i, 2) = 255;
-		color_wheel.at<double>(col + i, 0) = floor(255 * i / BM);
-	}
-	col += BM;
-
-	//MR calculation
-	for (int i = 0; i < MR; i++) {
-		color_wheel.at<double>(col + i, 2) = 255 - floor(255 * i / MR);
-		color_wheel.at<double>(col + i, 0) = 255;
-	}
-
-	Mat U_squared, V_squared, rad;
-	cv::pow(U, 2, U_squared);
-	cv::pow(V, 2, V_squared);
-	cv::sqrt(U_squared + V_squared, rad);
-
-	Mat a = Mat::zeros(U.rows, U.cols, CV_64FC1);
-
-	for (int i = 0; i < a.rows; i++) {
-		for (int j = 0; j < a.cols; j++) {
-			double v_element = V.at<double>(i, j);
-			double u_element = U.at<double>(i, j);
-			a.at<double>(i, j) = atan2(-v_element, -u_element) / M_PI;
-		}
-	}
-
-	Mat fk = (a + 1) / 2 * (num_cols - 1);
-	Mat k0 = Mat::zeros(U.rows, U.cols, CV_64FC1);
-
-	for (int i = 0; i < k0.rows; i++) {
-		for (int j = 0; j < k0.cols; j++) {
-			k0.at<double>(i, j) = floor(fk.at<double>(i, j));
-		}
-	}
-
-	Mat k1 = k0 + 1;
-
-	for (int i = 0; i < k1.rows; i++) {
-		for (int j = 0; j < k1.cols; j++) {
-			if (k1.at<double>(i, j) == num_cols)
-				k1.at<double>(i, j) = 0;
-		}
-	}
-
-	Mat f = fk - k0;
-	Mat f_prime = 1 - f;
-
-	vector<cv::Mat> channels;
-
-	for (int i = 0; i < color_wheel.cols; i++) {
-		Mat col0 = Mat::zeros(k0.rows, k0.cols, CV_64FC1);
-		Mat col1 = Mat::zeros(k1.rows, k1.cols, CV_64FC1);
-
-		for (int j = 0; j < k0.rows; j++) {
-			for (int k = 0; k < k0.cols; k++) {
-
-				double col0_index = k0.at<double>(j, k);
-				col0.at<double>(j, k) = color_wheel.at<double>(col0_index, i) / 255.0;
-
-				double col1_index = k1.at<double>(j, k);
-				col1.at<double>(j, k) = color_wheel.at<double>(col1_index, i) / 255.0;
-			}
-		}
-
-		Mat col_first, col_second, col;
-
-		multiply(f_prime, col0, col_first);
-		multiply(f, col1, col_second);
-
-		col = col_first + col_second;
-
-		for (int l = 0; l < col.rows; l++) {
-			for (int m = 0; m < col.cols; m++) {
-				if (rad.at<double>(l, m) <= 1) {
-					double col_val = 1 - rad.at<double>(l, m) * (1 - col.at<double>(l, m));
-					col.at<double>(l, m) = col_val;
-				}
-				else
-					col.at<double>(l, m) *= 0.75;
-			}
-		}
-		channels.push_back(col);
-	}
-	reverse(channels.begin(), channels.end());
-	cv::merge(channels, img);
-
-    img.convertTo(img, CV_8U, 255.0);
-	bitwise_not(img, img);
-    return img;
+	return flowVizualization(frame, flow, mask);
 }
 
 int main() {
-    VideoCapture cap("video/31.mp4");
-	VideoCapture capMask("video/31Mask.mp4");
-    VideoWriter res("video/test2.mp4", VideoWriter::fourcc('m','p','4','v'),
+    VideoCapture cap("video/orig.mp4");
+	// VideoCapture capMask("video/31Mask.mp4");
+    VideoWriter res("video/test1.mp4", VideoWriter::fourcc('m','p','4','v'),
                     cap.get(CAP_PROP_FPS), Size(cap.get(CAP_PROP_FRAME_WIDTH),
                                                 cap.get(CAP_PROP_FRAME_HEIGHT)));
     if(!cap.isOpened()){
@@ -387,15 +268,15 @@ int main() {
     while(true) {
         Mat frame, frame_mask;
         cap >> frame;
-		capMask >> frame_mask;
-		cvtColor(frame_mask, frame_mask, COLOR_BGR2GRAY);
+		// capMask >> frame_mask;
+		// cvtColor(frame_mask, frame_mask, COLOR_BGR2GRAY);
         if (frame.empty()) break;
         if (frameNum) {
 			Mat mask = Mat::zeros(frame.rows, frame.cols, CV_8U);
-            res.write(RobustLocalFlow(frameOld, frame, mask));
-			cout << diceCoefficient(frame_mask, mask) << ' ';
-			dice += diceCoefficient(frame_mask, mask);
-            // imshow("text", RobustLocalFlow(frameOld, frame));
+            res.write(HornSchunckFlow(frameOld, frame, mask));
+			// cout << diceCoefficient(frame_mask, mask) << ' ';
+			// dice += diceCoefficient(frame_mask, mask);
+            // imshow("text", HornSchunckFlow(frameOld, frame, mask));
             // waitKey();
         } else {
             magMask = vector(frame.rows, vector(frame.cols, false));
